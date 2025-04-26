dd98df79a6fa0ee28636ae76aab7aaf9784663af
BurakFatihOzen <brkfatih2016@gmail.com>
2025-04-27 02:14:27 +0300

    updated

--- app/src/main/java/com/example/odev1kotlin/MainActivity.kt
+++ app/src/main/java/com/example/odev1kotlin/MainActivity.kt
@@ -1,20 +1,104 @@
-package com.example.odev1kotlin
+package com.example.odev1kotlin.odev1
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+fun main() {
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+    val sehir = "Ankara"
+    val ulke = "Türkiye"
+    val telefon = "+90 555 123 45 67"
+    val postaKodu = "34000"
+    val email = "ornek@example.com"
+    val meslek = "Bilgisayar Mühendisi"
+    val stokMiktari = 150
+    val musteriAdi = "Burak Fatih"
+    val bakiye = 4500.75
+    val dogumGunu = "1995-04-20"
+    val maas = 25000
+    val medeniDurum = "Bekar"
+    val urunYorum = "Ürün harikaydı!"
+
+    val odemeTarihi = "2025-05-01"
+    val odeme = 999.99
+    val siparisAdeti = 3
+    val arabaModeli = "Alfa Romeo Giulietta"
+    val kitapAdi = "Martin Eden"
+    val yayinlamaTarihi = "2020-09-10"
+    val indirimMiktari = 25
+    val odaSayisi = 2
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val urunAdi = "Bluetooth Kulaklık"
+    val yemekFiyati = 120.5
+
+    val marka = "Samsung"
+    val muzikAdi = "Love"
+    val videoSuresi = "5:34"
+    val urunPuani = 4.8
+    val resimAdi = "manzara.jpg"
+    val dosyaFormati = "jpg"
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "S24 ULTRA"
+    val ekranBoyutu = 6.1
+    val agirlik = 178.5
+
+    val ulusalGun = "29 Ekim Cumhuriyet Bayramı"
+    val tatilGunu = "1 Mayıs Emek ve Dayanışma Günü"
+    val rezervasyonTarihi = "2025-07-15"
+    val sokakAdi = "198.sokak"
+    val otobusHatti = "34B"
+    val kalanDakika = 8
+    val takipKodu = "TR123456789"
+    val kuponSuresi = "2025-12-31"
+    val kuponKodu = "INDIRIM20"
+    val faturaAdresi = "Ankara,Söğütözü"
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorum: $urunYorum")
+
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ağırlık: $agirlik")
+
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
 }
